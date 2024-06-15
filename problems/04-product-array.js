8a0b083ee40bbd46ef22e4fe2e2d18a52f183095
jnorm23 <joshua_norman11@hotmail.com>
2024-06-15 13:34:16 -0400

    Question 4 answer

--- problems/04-product-array.js
+++ problems/04-product-array.js
@@ -5,6 +5,13 @@ together. You can assume that nums will not be an empty array.
 */
 
 // Your code here 
+function product(nums){
+    let product = 1;
+    for (let i = 0; i < nums.length; i++) {
+        product *= nums[i];
+    }
+    return product;
+}
 
 
 // console.log(product([10, 3, 5, 2])); // 300
