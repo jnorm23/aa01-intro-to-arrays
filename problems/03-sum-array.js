0078a42937ef5984b7e838d68380547b5c509044
jnorm23 <joshua_norman11@hotmail.com>
2024-06-15 13:25:53 -0400

    Question 3 answer

--- problems/03-sum-array.js
+++ problems/03-sum-array.js
@@ -3,7 +3,13 @@ returns the total sum of all the numbers.
 */
 
 // Your code here 
-
+function sumArray(arr){
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+    }
+    return total;
+}
 
 // console.log(sumArray([1, 2]));        // => 3
 // console.log(sumArray([5, 6, 4]));     // => 15
