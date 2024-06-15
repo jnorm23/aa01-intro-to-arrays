7a2e81e5cb48eafd6d0cdeb998a78c2cf7cb08f8
jnorm23 <joshua_norman11@hotmail.com>
2024-06-15 13:21:29 -0400

    Question 2 answer

--- problems/02-first-and-last.js
+++ problems/02-first-and-last.js
@@ -7,6 +7,18 @@ should return the difference between the first and last elements of the array.
 
 // Your code here 
 
+function firstAndLast(arr){
+if (arr.length === 0)
+    return 0
+    const first = arr[0]
+    const last = arr[arr.length - 1]
+if (arr.length % 2 === 0) {
+    return first + last
+} else {
+    return first - last
+}
+}
+    
 
 // console.log(firstAndLast([1, 2, 3, 4]));    // 5
 // console.log(firstAndLast([1, 2, 3, 4, 5])); // -4
